e60a331de65f49f71d12350f15db90eb58cf3a4d
xqc950217 <1006866091@qq.com>
2019-11-30 14:33:23 +0800

    项目初始化-配置 REM 适配

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './utils/register-vant'
+import 'amfe-flexible'
 Vue.config.productionTip = false
 
 new Vue({
