6283ffcd4008eba85a7bc71508013cc26ac27724
xqc950217 <1006866091@qq.com>
2019-11-30 19:42:23 +0800

    实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,28 +5,51 @@
     <!-- 表单 -->
     <van-cell-group>
       <van-field
+        v-model="user.mobile"
         label="手机号"
         placeholder="请输入用户名"
       />
       <van-field
+        v-model="user.code"
         label="验证码"
         placeholder="请输入验证码"  />
     </van-cell-group>
     <!-- 登录按钮 -->
     <div class="login-box">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click="onLogin">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
 export default {
   name: 'LoginPage',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    async onLogin () {
+      try {
+        const res = await request({
+          method: 'post',
+          url: '/app/v1_0/authorizations',
+          data: this.user
+        })
+        console.log('登陆成功', res)
+      } catch (err) {
+        console.log('登录失败', err)
+      }
+    }
   }
+
 }
 </script>
 
