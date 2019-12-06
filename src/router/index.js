442f8849e258aa8705047388f31d4248b133d8d8
xqc950217 <1006866091@qq.com>
2019-12-06 18:21:37 +0800

    创建搜索结果组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '@/views/login'
 import Tabbar from '@/views/tabbar'
 import Home from '@/views/home'
 import Search from '@/views/search'
+import SearchResult from '@/views/search-result'
 Vue.use(VueRouter)
 // 配置路由表
 const routes = [
@@ -24,6 +25,10 @@ const routes = [
   {
     path: '/search',
     component: Search
+  },
+  {
+    path: '/search/:q',
+    component: SearchResult
   }
 
 ]
---
+++ src/views/search-result/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      搜索结果
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResult',
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
